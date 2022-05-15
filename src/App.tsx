c79a1005182784c787719f551f9a5223e51cfe8f
wael65 <waelebrahim65@gmail.com>
2022-05-15 15:23:38 +0200

    make change in screen message

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is from App.tsx
         </a>
       </header>
     </div>
